80e2eee86391d45f72306c936e5d062d3431b890
cyb_tachyon <cyb.tachyon@gmail.com>
2017-08-10 14:32:06 -0400

    Move JSON Web Token validation to OpenshiftApi

--- OpenshiftApi.php
+++ OpenshiftApi.php
@@ -19,6 +19,30 @@ use Guzzle\Http\ClientInterface;
 class OpenshiftApi {
 
   /**
+   * Openshift JWT required keys.
+   *
+   * @var array
+   */
+  public static $jwtKeys = array(
+    0 => array(
+      'header' => array(
+        'alg',
+        'typ',
+      ),
+    ),
+    1 => array(
+      'payload' => array(
+        'iss',
+        'kubernetes.io/serviceaccount/namespace',
+        'kubernetes.io/serviceaccount/secret.name',
+        'kubernetes.io/serviceaccount/service-account.name',
+        'kubernetes.io/serviceaccount/service-account.uid',
+        'sub',
+      ),
+    ),
+  );
+
+  /**
    * The base64 encoded JSON Web Token secret.
    *
    * @var string
@@ -72,6 +96,48 @@ class OpenshiftApi {
    */
   public function getSecret() {
     return $this->apiSecret;
+  }
+
+  /**
+   * Validates a JSON Web Token as much as possible without the key.
+   *
+   * @param string $jwt
+   *   The JSON Web Token string to validate.
+   *
+   * @return array
+   *   An array of validation errors, or an empty array if passing.
+   */
+  public static function validateJwt($jwt) {
+    $errors = array();
+    if (!empty($jwt) && !preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $jwt)) {
+      $jwt_parts = explode('.', $jwt);
+      if (count($jwt_parts) < 3) {
+        $errors[] = t('The secret is not a JSON Web Token - make sure all three parts are included.');
+        return $errors;
+      }
+      /** @var array $jwt_part */
+      foreach (self::$jwtKeys as $index => $jwt_part) {
+        $part_names = array_keys($jwt_part);
+        $part_name = reset($part_names);
+        $part_decoded = base64_decode($jwt_parts[$index], TRUE);
+        if (trim(@base64_encode($part_decoded), '=') !== $jwt_parts[$index]) {
+          $errors[] = t("The JWT %part does not appear to be base64 encoded, perhaps use '%encode'?.",
+            array(
+              '%part' => $part_name,
+              '%encode' => base64_encode($jwt_parts[$index]),
+            ));
+          continue;
+        }
+        $part_data = drupal_json_decode($part_decoded);
+        foreach (reset($jwt_part) as $part_key) {
+          if (!isset($part_data[$part_key])) {
+            $errors[] = t('The JWT Header is missing the %key property.',
+              array('%key' => $part_key));
+          }
+        }
+      }
+    }
+    return $errors;
   }
 
 }
