244c141f910d4fc386dc7b456d2c486cb6573934
cyb_tachyon <cyb.tachyon@gmail.com>
2017-08-10 14:32:35 -0400

    Add OpenShift image stream, build config methods

--- OpenshiftApi.php
+++ OpenshiftApi.php
@@ -99,6 +99,353 @@ class OpenshiftApi {
   }
 
   /**
+   * Methods relating to OpenShift build configs.
+   *
+   * @ingroup build_config
+   * @{
+   */
+
+  /**
+   * Creates a new OpenShift build config.
+   *
+   * @param string $pid
+   *   The OpenShift project ID.
+   * @param array $config
+   *   The build config description array to create.
+   * @param bool $use_defaults
+   *   Set to FALSE to avoid using common default settings.
+   *
+   * @return bool
+   *   TRUE if the request succeeded.
+   *
+   * @throws RuntimeException
+   *   Signifies an issue has occurred generating an HTTP Request.
+   *
+   * @code
+   * $config = array(
+   *   'metadata' => array(
+   *     'name' => 'my-buildconfig',
+   *   ),
+   *   'spec' => array(
+   *     'source' => array(
+   *       'type' => 'Git',
+   *       'git' => array(
+   *         'url' => 'https://github.com/openshift/ruby-hello-world',
+   *         'ref' => 'master',
+   *     ),
+   *     'output' => array(
+   *       'to' => array(
+   *         'kind' => 'ImageStreamTag',
+   *         'name' => 'my-builds:1',
+   *       ),
+   *     ),
+   *   ),
+   * );
+   * $osa->createBuildConfig('p12345', $config);
+   * @endcode
+   *
+   * @see https://docs.openshift.org/latest/rest_api/openshift_v1.html#v1-buildconfig
+   * @see https://docs.openshift.org/latest/rest_api/openshift_v1.html#create-a-buildconfig-2
+   */
+  public function createBuildConfig($pid, array $config, $use_defaults = TRUE) {
+    $headers = array(
+      'Content-Type' => 'application/json',
+    );
+    $defaults = array(
+      'kind' => 'BuildConfig',
+      'apiVersion' => 'v1',
+      'metadata' => array(),
+      'spec' => array(),
+    );
+    $defaults_spec = array(
+      'runPolicy' => 'Serial',
+      'source' => array(),
+      'strategy' => array(
+        'type' => 'Docker',
+      ),
+    );
+    if ($use_defaults) {
+      $config += $defaults;
+      $config['spec'] += $defaults_spec;
+    }
+    $request = $this->client->post("namespaces/$pid/buildconfigs", $headers, drupal_json_encode($config));
+    try {
+      $request->send();
+    }
+    catch (RuntimeException $exception) {
+      throw $exception;
+    }
+    return ($request->getResponse() !== NULL && $request->getResponse()->getStatusCode() === 201);
+  }
+
+  /**
+   * Returns the build config for a specific OpenShift project ID.
+   *
+   * @param string $pid
+   *   The OpenShift project ID to query build configs against.
+   * @param string $build_id
+   *   The OpenShift build ID string.
+   *
+   * @return bool
+   *   Returns TRUE if the operation succeeded.
+   *
+   * @throws RuntimeException
+   *   Signifies an issue has occurred generating an HTTP Request.
+   */
+  public function deleteBuildConfig($pid, $build_id) {
+    $request = $this->client->delete("namespaces/$pid/buildconfigs/$build_id");
+    try {
+      $request->send();
+    }
+    catch (RuntimeException $exception) {
+      throw $exception;
+    }
+    return ($request->getResponse() !== NULL && $request->getResponse()->getStatusCode() === 200);
+  }
+
+  /**
+   * Returns the build config for a specific OpenShift project ID.
+   *
+   * @param string $pid
+   *   The OpenShift project ID to query build configs against.
+   * @param string $build_id
+   *   The OpenShift build ID string.
+   *
+   * @return array
+   *   A build config description array.
+   *
+   * @throws RuntimeException
+   *   Signifies an issue has occurred generating an HTTP Request.
+   */
+  public function getBuildConfig($pid, $build_id) {
+    $request = $this->client->get("namespaces/$pid/buildconfigs/$build_id");
+    try {
+      $response = $request->send();
+    }
+    catch (RuntimeException $exception) {
+      if ($request->getResponse() !== NULL && $request->getResponse()->getStatusCode() === 404) {
+        return array();
+      }
+      throw $exception;
+    }
+    if ($body = $response->getBody()) {
+      return drupal_json_decode($body);
+    }
+    return array();
+  }
+
+  /**
+   * Returns the build config for a specific OpenShift project ID.
+   *
+   * @param string $pid
+   *   The OpenShift project ID to query build configs against.
+   *
+   * @return array
+   *   A build config description array.
+   *
+   * @throws RuntimeException
+   *   Signifies an issue has occurred generating an HTTP Request.
+   */
+  public function getBuildConfigs($pid) {
+    $request = $this->client->get("namespaces/$pid/builds");
+    try {
+      $response = $request->send();
+    }
+    catch (RuntimeException $exception) {
+      throw $exception;
+    }
+    if ($body = $response->getBody()) {
+      $data = drupal_json_decode($body);
+      return isset($data['items']) ? $data['items'] : array();
+    }
+    return array();
+  }
+
+  /**
+   * Sets a build config's updated configuration.
+   *
+   * @param string $pid
+   *   The OpenShift project ID.
+   * @param string $config_name
+   *   The OpenShift build config name tag.
+   * @param array $config
+   *   The build config description array to update.
+   *
+   * @return bool
+   *   TRUE if the build configuration was updated successfully.
+   *
+   * @throws RuntimeException
+   *   Signifies an issue has occurred generating an HTTP Request.
+   *
+   * @code
+   * $config = array(
+   *   'spec' => array(
+   *     'source' => array(
+   *       'contextDir' => '',
+   *       'git' => array(
+   *         'url' => 'https://github.com/openshift/ruby-hello-world',
+   *         'ref' => 'beta4',
+   *     ),
+   *   ),
+   * );
+   * $osa->createBuildConfig('p12345', $config);
+   * @endcode
+   *
+   * @see https://docs.openshift.org/latest/rest_api/openshift_v1.html#v1-buildconfig
+   */
+  public function setBuildConfig($pid, $config_name, array $config) {
+    $headers = array(
+      'Content-Type' => 'application/strategic-merge-patch+json',
+    );
+    $request = $this->client->patch(
+      "namespaces/$pid/buildconfigs/$config_name",
+      $headers,
+      drupal_json_encode($config));
+    try {
+      $request->send();
+    }
+    catch (RuntimeException $exception) {
+      throw $exception;
+    }
+    return ($request->getResponse() !== NULL && $request->getResponse()->getStatusCode() === 200);
+  }
+
+  /**
+   * @}
+   *
+   * Methods related to Open Shift Image Streams.
+   *
+   * @ingroup image_stream
+   */
+
+  /**
+   * Creates a new OpenShift image stream.
+   *
+   * @param string $pid
+   *   The OpenShift project ID.
+   * @param array $config
+   *   The build config description array to create.
+   * @param bool $use_defaults
+   *   Set to FALSE to avoid using common default settings.
+   *
+   * @return bool
+   *   TRUE if the image stream was created successfully.
+   *
+   * @throws RuntimeException
+   *   Signifies an issue has occurred generating an HTTP Request.
+   *
+   * @code
+   * $config = array(
+   *   'metadata' => array(
+   *     'name' => 'my-builds',
+   *   ),
+   * );
+   * $osa->createBuildConfig('p12345', $config);
+   * @endcode
+   *
+   * @see https://docs.openshift.org/latest/rest_api/openshift_v1.html#v1-imagestream
+   * @see https://docs.openshift.org/latest/rest_api/openshift_v1.html#create-an-imagestream
+   */
+  public function createImageStream($pid, array $config, $use_defaults = TRUE) {
+    $headers = array(
+      'Content-Type' => 'application/json',
+    );
+    $defaults = array(
+      'kind' => 'ImageStream',
+      'apiVersion' => 'v1',
+      'metadata' => array(
+        'creationTimestamp' => REQUEST_TIME,
+        'labels' => array(
+          'build' => $config['metadata']['name'],
+        ),
+      ),
+      'spec' => array(),
+      'status' => array(
+        'dockerImageRepository' => '',
+      ),
+    );
+    if ($use_defaults) {
+      $config += $defaults;
+    }
+    $request = $this->client->post("namespaces/$pid/imagestreams", $headers, drupal_json_encode($config));
+    try {
+      $request->send();
+    }
+    catch (RuntimeException $exception) {
+      throw $exception;
+    }
+    return ($request->getResponse() !== NULL && $request->getResponse()->getStatusCode() === 201);
+  }
+
+  /**
+   * Gets an OpenShift Image Stream by project and name.
+   *
+   * @param string $pid
+   *   The OpenShift project ID.
+   * @param string $stream_id
+   *   The OpenShift image stream ID.
+   *
+   * @return array
+   *   An array of OpenShift image streams.
+   *
+   * @throws RuntimeException
+   *   Signifies an issue has occurred generating an HTTP Request.
+   *
+   * @see https://docs.openshift.org/latest/rest_api/openshift_v1.html#list-or-watch-objects-of-kind-imagestream
+   */
+  public function getImageStream($pid, $stream_id) {
+    $request = $this->client->get("namespaces/$pid/imagestreams/$stream_id");
+    try {
+      $response = $request->send();
+    }
+    catch (RuntimeException $exception) {
+      if ($request->getResponse() !== NULL && $request->getResponse()->getStatusCode() === 404) {
+        return array();
+      }
+      throw $exception;
+    }
+    if ($body = $response->getBody()) {
+      return drupal_json_decode($body);
+    }
+    return array();
+  }
+
+  /**
+   * Gets a list of OpenShift Image Streams by project.
+   *
+   * @param string $pid
+   *   The OpenShift project ID.
+   *
+   * @return array
+   *   An array of OpenShift image streams.
+   *
+   * @throws RuntimeException
+   *   Signifies an issue has occurred generating an HTTP Request.
+   *
+   * @see https://docs.openshift.org/latest/rest_api/openshift_v1.html#list-or-watch-objects-of-kind-imagestream
+   */
+  public function getImageStreams($pid) {
+    $request = $this->client->get("namespaces/$pid/imagestreams");
+    try {
+      $response = $request->send();
+    }
+    catch (RuntimeException $exception) {
+      throw $exception;
+    }
+    if ($body = $response->getBody()) {
+      $data = drupal_json_decode($body);
+      return isset($data['items']) ? $data['items'] : array();
+    }
+    return array();
+  }
+
+  /**
+   * @}
+   *
+   * @ingroup utility
+   */
+
+  /**
    * Validates a JSON Web Token as much as possible without the key.
    *
    * @param string $jwt
